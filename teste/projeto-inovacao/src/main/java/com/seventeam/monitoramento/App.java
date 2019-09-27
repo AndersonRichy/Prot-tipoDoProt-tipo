4ea4b435ffa99e57e5cf6625b29604b3543755d6
AndersonRichy <47605526+AndersonRichy@users.noreply.github.com>
2019-09-26 21:21:48 -0300

    Add files via upload

---
+++ teste/projeto-inovacao/src/main/java/com/seventeam/monitoramento/App.java
@@ -0,0 +1,13 @@
+package com.seventeam.monitoramento;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ teste/projeto-inovacao/src/main/java/com/seventeam/monitoramento/TesteMonitoramento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.seventeam.monitoramento;
+
+import oshi.hardware.platform.windows.WindowsCentralProcessor;
+
+/**
+ *
+ * @author Aluno
+ */
+public class TesteMonitoramento {
+    
+    
+    public void Monitor() {   
+    
+        WindowsCentralProcessor MonitoramentoCPU = new WindowsCentralProcessor();
+        
+    }
+}
